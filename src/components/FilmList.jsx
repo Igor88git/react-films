b0005b3e5918b834a2432a175340db2d65b72e9e
IgorZakutajlo <87570302+igorcodes@users.noreply.github.com>
2021-07-25 16:22:58 +0300

    Update FilmList.jsx

--- src/components/FilmList.jsx
+++ src/components/FilmList.jsx
@@ -3,7 +3,7 @@ import {Film} from './Film'
 function FilmList(props) {
     const{ movies = [] } = props; // если ничего не найдено, придет пустой массив по умолчанию
 
-    //проходит по всем фильмам что есть и через map для каждого фильма делает разметку, можно сделать спред опрератор
+    //через map для каждого фильма разметку, можно сделать спред опрератор
     return <div className='movies'>
         {movies.length ? movies.map(movie => (
             <Film key={movie.imdbID} 
